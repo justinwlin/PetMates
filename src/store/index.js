6243b25eb86216a6b4e5c9a79e6a1f65e5c8660d
Justin Lin <justinlin@justins-mbp-5.lan>
2021-03-07 15:28:13 -0800

    Adding basic Vuex and login screen functionality

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    loginUserState: false
   },
   mutations: {
+    loginMutation(state) {
+      console.log('login');
+      state.loginUserState = true;
+    }
   },
   actions: {
   },
+  getters: {
+    loggedInState(state) {
+      return state.loginUserState;
+    }
+  },
   modules: {
   }
-})
+});
