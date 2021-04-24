2230bf83234fb6f3cc1cae5f1f117b6f08c80980
gianayang <gianayang31@gmail.com>
2021-04-24 18:28:28 -0400

    new changes

--- src/views/ShelterSelected.vue
+++ src/views/ShelterSelected.vue
@@ -1,30 +1,29 @@
 <template>
-    <div class = "home">
-        <div class="navBar">
-            <el-button type="primary">Select Shelter</el-button>
-            <el-button type="primary">Favorites</el-button>
-        </div>
-        <div class="picture">
-            <img v-bind:src="shelterImage" />
-        </div>
-        <div class = "profile">
-            <h1>{{shelterName}}
-            <el-button>Email Shelter </el-button></h1> 
-            <div class = "like">
-                <p>LIKES : {{likes}}         DISLIKES: {{dislikes}}</p>
-            </div>
-            <p>
-                {{shelterDescription }} <br/>
-                {{shelterState }}<br/>
-                {{shelterStreet }}<br/>
-                {{shelterCity }}<br/>
-                {{shelterZipcode }}<br/>
-            </p>
-            <div class = "pet">
-                <el-button type="primary">CHECK OUT THE PETS</el-button>
-            </div>
-        </div>
-    </div>  
+  <div class="home">
+    <div class="navBar">
+      <el-button type="primary">Select Shelter</el-button>
+      <el-button type="primary">Favorites</el-button>
+    </div>
+    <div class="picture">
+      <img v-bind:src="shelterImage" />
+    </div>
+    <div class="profile">
+      <h1>{{ shelterName }} <el-button>Email Shelter </el-button></h1>
+      <div class="like">
+        <p>LIKES : {{ likes }} DISLIKES: {{ dislikes }}</p>
+      </div>
+      <p>
+        {{ shelterDescription }} <br />
+        {{ shelterState }}<br />
+        {{ shelterStreet }}<br />
+        {{ shelterCity }}<br />
+        {{ shelterZipcode }}<br />
+      </p>
+      <div class="pet">
+        <el-button type="primary">CHECK OUT THE PETS</el-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -34,56 +33,56 @@ export default {
   data() {
     return {
       shelterImage: "",
-      shelterName:"",
+      shelterName: "",
       shelterDescription: "",
       shelterState: "",
       shelterStreet: "",
       shelterCity: "",
       shelterZipcode: "",
-      likes:"",
-      dislikes:"",
+      likes: "",
+      dislikes: "",
     };
   },
-  created() {
-    (async () => {
-      const shelterSnapshot = await this.$store.dispatch("getShelter", {shelterID: 1,});
-      if (shelterSnapshot.empty) {
-        console.log("No such shelter document!");
-      } else {
-        shelterSnapshot.forEach((doc) => {
-          const data = doc.data();
-          this.shelterName = data.name;
-          this.shelterState = data.state;
-          this.shelterStreet = data.street;
-          this.shelterCity = data.city;
-          this.shelterZipcode = data.zipcode;
-          this.likes = data.likes;
-          this.dislikes = data.dislikes;
-          this.shelterImage = data.image;
-       });
-      }
-    })();
+  async created() {
+    const shelterSnapshot = await this.$store.dispatch("getShelter", {
+      shelterID: 1,
+    });
+    if (shelterSnapshot.empty) {
+      console.log("No such shelter document!");
+    } else {
+      shelterSnapshot.forEach((doc) => {
+        const data = doc.data();
+        this.shelterName = data.name;
+        this.shelterState = data.state;
+        this.shelterStreet = data.street;
+        this.shelterCity = data.city;
+        this.shelterZipcode = data.zipcode;
+        this.likes = data.likes;
+        this.dislikes = data.dislikes;
+        this.shelterImage = data.image;
+      });
+    }
   },
 };
 </script>
 
 <style scoped>
-    .picture{
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        position: absoulte;
-        margin-top: 100px;
-        width: 50%;
-    }
-    .profile{
-        width: 100%;
-        text-align:left; 
-        margin-left: 450px;
-        position:absolute;
-        display: inline-block;
-    }
-    .pet{
-        text-align:left;
-    }
+.picture {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  position: absoulte;
+  margin-top: 100px;
+  width: 50%;
+}
+.profile {
+  width: 100%;
+  text-align: left;
+  margin-left: 450px;
+  position: absolute;
+  display: inline-block;
+}
+.pet {
+  text-align: left;
+}
 </style>
