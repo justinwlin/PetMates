6dc01573a6723851c9afbcdd223ad99939d32ebc
Justin Lin <justinlin@justins-mbp-5.lan>
2021-04-24 16:57:52 -0700

    LandingPage

--- src/views/landingPage.vue
+++ src/views/landingPage.vue
@@ -1,23 +1,33 @@
 <template>
-  <div class="home">hi</div>
+  <el-menu
+    :default-active="activeIndex2"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <el-menu-item index="1" @click="homeNavigation">Login</el-menu-item>
+    <el-menu-item index="2" @click="favoriteNavigation">Register your Shelter</el-menu-item>
+  </el-menu>
+  <div class="home"></div>
 </template>
 
 <script>
 export default {
   name: "Home",
   components: {},
-  data() {
-    return {
-      shelterImage: "",
-      shelterName: "",
-      shelterDescription: "",
-      shelterState: "",
-      shelterStreet: "",
-      shelterCity: "",
-      shelterZipcode: "",
-      likes: "",
-      dislikes: "",
-    };
+  data() {},
+  methods: {
+    homeNavigation() {
+      this.$store.commit("navigatePage", { page: "regularlogin" });
+      this.active = 1;
+    },
+    favoriteNavigation() {
+      this.$store.commit("navigatePage", { page: "shelterregistration" });
+      this.active = 2;
+    },
   },
 };
 </script>
@@ -26,7 +36,8 @@ export default {
 .home {
   height: 100vh;
   width: 100vw;
-  background: url("https://i.ytimg.com/vi/0Zo3ES9kuDs/maxresdefault.jpg")
-    no-repeat center center fixed;
+  background: url("./../assets/landingPage.png");
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
