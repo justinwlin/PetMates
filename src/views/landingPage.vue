478834ee9dbe685593fdd1e8b94d3c4400c1721a
Justin Lin <justinlin@justins-mbp-5.lan>
2021-04-24 16:49:23 -0700

    landing page

---
+++ src/views/landingPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">hi</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      shelterImage: "",
+      shelterName: "",
+      shelterDescription: "",
+      shelterState: "",
+      shelterStreet: "",
+      shelterCity: "",
+      shelterZipcode: "",
+      likes: "",
+      dislikes: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  height: 100vh;
+  width: 100vw;
+  background: url("https://i.ytimg.com/vi/0Zo3ES9kuDs/maxresdefault.jpg")
+    no-repeat center center fixed;
+}
+</style>
